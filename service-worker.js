1cefedf18310018db05b7b39d22060a0439e7446
skas <skaserayamangalam@gmail.com>
2026-02-01 11:02:29 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,7 @@
-const CACHE_NAME = "finance-pwa-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html"
-];
-
-// Install SW and cache content
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open("finance-cache-v1").then(cache => cache.addAll(["/", "/index.html"])));
 });
 
-// Serve cached assets
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
